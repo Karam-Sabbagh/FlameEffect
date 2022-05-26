9d7ffa513ed4ac76854c95fc88f93e62da4e504e
InZaneGamer84 <InZaneGamer84@protonmail.com>
2022-05-26 06:02:28 -0400

    function draw_points

--- flame_effect.py
+++ flame_effect.py
@@ -1,15 +1,25 @@
 import os
-from turtle import width
+
 import pygame
+from pygame.locals import *
 
 # TODO: Setup pixel data structure...
 
 background_color = (0, 0, 0)
-(width, height) = (600, 600)
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 500
+WINDOW_SURFACE = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE
 
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Flame Effect")
-screen.fill(background_color)
+
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE)
+pygame.display.set_caption("Fire Effect")
+
+def draw_points(points: list):
+
+    for p in points:
+        WINDOW_SURFACE.set_at((p[x], p[y]), my_color)
+
 
 pygame.display.flip()
 
